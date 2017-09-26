1e74180eb868f2c0901e76faaa47b167c5ab65d6
Brian Kassouf <bkassouf@hashicorp.com>
2017-09-26 16:33:31 -0700

    Add a note about falling back to the login JWT

--- path_config.go
+++ path_config.go
@@ -29,8 +29,10 @@ func pathConfig(b *kubeAuthBackend) *framework.Path {
 				Description: "PEM encoded CA cert for use by the TLS client used to talk with the API.",
 			},
 			"token_reviewer_jwt": {
-				Type:        framework.TypeString,
-				Description: "A service account JWT used to access the TokenReview API to validate other JWTs during login.",
+				Type: framework.TypeString,
+				Description: `A service account JWT used to access the
+TokenReview API to validate other JWTs during login. If not set
+the JWT used for login will be used to access the API.`,
 			},
 			"pem_keys": {
 				Type: framework.TypeCommaStringSlice,
