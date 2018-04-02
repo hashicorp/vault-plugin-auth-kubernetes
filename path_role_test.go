07d4bf28da1bc9612999317f83f3bc6786b1f9ae
Jim Kalafut <jkalafut@hashicorp.com>
2018-04-02 15:04:11 -0700

    Replace logxi with hclog

--- path_role_test.go
+++ path_role_test.go
@@ -6,9 +6,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/hashicorp/vault/helper/logging"
+	log "github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/vault/logical"
-	log "github.com/mgutz/logxi/v1"
 )
 
 func getBackend(t *testing.T) (logical.Backend, logical.Storage) {
@@ -17,7 +16,8 @@ func getBackend(t *testing.T) (logical.Backend, logical.Storage) {
 	b := Backend()
 
 	config := &logical.BackendConfig{
-		Logger: logging.NewVaultLogger(log.LevelTrace),
+		Logger: log.New(&log.LoggerOptions{Level: log.Trace}),
+
 		System: &logical.StaticSystemView{
 			DefaultLeaseTTLVal: defaultLeaseTTLVal,
 			MaxLeaseTTLVal:     maxLeaseTTLVal,
